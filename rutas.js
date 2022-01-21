1f4df9d52a42f711ebf527ec6a4700f27bd24c86
AgustinEGarcia <agustinezequiel1997@gmail.com>
2022-01-20 21:05:56 -0300

    metodo post

--- rutas.js
+++ rutas.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(express.json());
+
 let notas = [
     {
       id: 1,
@@ -46,6 +48,31 @@ router.delete('/api/notas/:id', (req, res) => {
   notas = notas.filter(nota => nota.id !== id);
   res.json(notas);
   res.status(204).end();
+});
+
+
+router.post('/api/notas', (req, res) => {
+  const nota = req.body;
+
+  if(!nota || !nota.content){
+    return res.status(400).json({
+      error: 'nota.content no tiene contenido'
+    });
+  };
+
+  const ids = notas.map(nota => nota.id);
+  const maxId = Math.max(...ids);
+
+  const nuevaNota = {
+    id: maxId + 1,
+    content: nota.content,
+    important: typeof nota.important !== 'undefined' ? nota.important : false,
+    date: new Date().toISOString()
+  }
+
+  notas = [...notas, nuevaNota]; //o tambien se puede hacer notas.concat(nuevaNota);
+
+  res.status(201).json(nuevaNota);
 });
 
 module.exports = router;
